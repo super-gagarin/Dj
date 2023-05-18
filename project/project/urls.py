2be2fe5503d4a0e08d242e80b20bf25dca940b7a
Gagarin <super-gagarin@yandex.ru>
2023-05-18 07:26:48 +0300

    Initial Commit

--- project/project/urls.py
+++ project/project/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from .views import redirect_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
+    path('', redirect_view)
 ]
